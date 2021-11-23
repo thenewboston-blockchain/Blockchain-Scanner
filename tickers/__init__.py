4b89543cbec2b5cb43d9d7d9b11b57c2667b4bcd
Oleg Tropinin <oleg@iptech.kz>
2021-11-23 23:11:09 +0600

    Add Nano blockchain. (#2)

--- tickers/__init__.py
+++ tickers/__init__.py
@@ -1,5 +1,6 @@
 from typing import TypeVar, Type, Optional
 
+from exceptions import ValidationError
 from tickers.utils import import_from_string
 
 T = TypeVar('T', bound='BaseTicker')
@@ -8,8 +9,9 @@ T = TypeVar('T', bound='BaseTicker')
 class BaseTicker:
     confirmations_count = None
 
-    def __init__(self, connection_string: str):
-        raise NotImplementedError('Must be implemented in a child class')
+    def __init__(self, connection_string: str, receiving_address: str, amount: float):
+        self.receiving_address = receiving_address
+        self.amount = amount
 
     @classmethod
     def get_instance(cls: Type[T], blockchain_type: str) -> T:
@@ -19,9 +21,16 @@ class BaseTicker:
         raise NotImplementedError('Must be implemented in a child class')
 
     @staticmethod
-    def make_configuration(rpc_user: Optional[str], rpc_password: Optional['str'], ip_address: Optional[str],
+    def make_configuration(username: Optional[str], password: Optional['str'], ip_address: Optional[str],
                            port: Optional[int]):
-        raise NotImplementedError('Must be implemented in a child class')
+        credentials = ''
+        if username:
+            credentials += username
+        if password:
+            credentials += f':{password}'
+        if credentials:
+            credentials += '@'
+        return dict(connection_string=f'http://{credentials}{ip_address}:{port}')
 
     def get_transaction(self, transaction_hash: str):
         raise NotImplementedError('Must be implemented in a child class')
@@ -29,5 +38,23 @@ class BaseTicker:
     def get_last_block_number(self) -> int:
         raise NotImplementedError('Must be implemented in a child class')
 
-    def was_transaction_confirmed(self, transaction_hash: str) -> bool:
+    def was_transaction_confirmed(self, transaction: dict) -> bool:
+        raise NotImplementedError('Must be implemented in a child class')
+
+    def validate_transaction(self, transaction: dict, amount: float, receiving_address: str):
+        errors = []
+        if transaction is None:
+            errors.append('tx hash is not present in blockchain')
+        else:
+            if not self.validate_receiving_address(transaction, receiving_address):
+                errors.append('Destination address of transaction is invalid.')
+            if not self.validate_amount(transaction, amount):
+                errors.append('Amount is incorrect.')
+        if errors:
+            raise ValidationError(errors)
+
+    def validate_amount(self, transaction: dict, amount: float):
+        raise NotImplementedError('Must be implemented in a child class')
+
+    def validate_receiving_address(self, transaction: dict, receiving_address: str):
         raise NotImplementedError('Must be implemented in a child class')
--- tickers/bitcoin.py
+++ tickers/bitcoin.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from bitcoinrpc.authproxy import AuthServiceProxy
 
 from tickers import BaseTicker
@@ -11,14 +9,14 @@ class BitcoinTicker(BaseTicker):
     def __init__(self, connection_string):
         self.rpc_connection = AuthServiceProxy(connection_string)
 
-    @staticmethod
-    def make_configuration(rpc_user: Optional[str], rpc_password: Optional['str'], ip_address: Optional[str],
-                           port: Optional[int]):
-        return dict(connection_string=f'http://{rpc_user}:{rpc_password}@{ip_address}:{port}')
-
     def get_transaction(self, transaction_hash: str):
         return self.rpc_connection.gettxout(transaction_hash, 0)
 
-    def was_transaction_confirmed(self, transaction_hash: str) -> bool:
-        transaction = self.get_transaction(transaction_hash)
+    def was_transaction_confirmed(self, transaction: dict) -> bool:
         return transaction['confirmations'] >= self.confirmations_count
+
+    def validate_amount(self, transaction: dict, amount: float):
+        return float(transaction['value']) == amount
+
+    def validate_receiving_address(self, transaction: dict, receiving_address: str):
+        return transaction['scriptPubKey']['address'] == receiving_address
